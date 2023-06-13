d34263b95cc7a34738428d07c5f08371118b3277
PratikshaAghav17 <88248358+PratikshaAghav17@users.noreply.github.com>
2023-06-13 10:06:23 +0530

    Calculate the average product price by category

---
+++ Query11.sql
@@ -0,0 +1,7 @@
+#11. Calculate the average product price by category
+SELECT Categories.Category_ID, Categories.Category_Name, AVG(Products.Unit_Price) AS AveragePrice
+FROM Categories
+JOIN Products ON Categories.Category_ID = Products.Category_ID
+GROUP BY Categories.Category_ID, Categories.Category_Name;
+
+#This query involves joining the Categories and Products tables on the CategoryID column. The AVG() function is used to calculate the average product price by category, using the UnitPrice column from the Products table.
